87ab6655b93841473a336afde65a8dbf844c1a70
TharudiHS <tharudihimeshaaa@gmail.com>
2025-11-18 12:46:10 +0530

    added new text styles

---
+++ lib/utils/text_styles.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'app_colours.dart';
+
+class TextStyles {
+  static const TextStyle size20Eurostileregular = TextStyle(
+    fontFamily: 'Eurostile',
+    fontSize: 20,
+    fontWeight: FontWeight.w400, //regular
+    color: AppColours.white,
+  );
+  static const TextStyle size16Promptregular = TextStyle(
+    fontFamily: 'Prompt',
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColours.gold85,
+  );
+  static const TextStyle size16Promptmedium = TextStyle(
+    fontFamily: 'Prompt',
+    fontSize: 16,
+    fontWeight: FontWeight.w500, //medium
+    color: AppColours.gold,
+  );
+  static const TextStyle size16Promptmediumwhite = TextStyle(
+    fontFamily: 'Prompt',
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: AppColours.white,
+  );
+  static const TextStyle size16Promptmediumgold85 = TextStyle(
+    fontFamily: 'Prompt',
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColours.gold85,
+  );
+  static const TextStyle size12Promptregular = TextStyle(
+    fontFamily: 'Prompt',
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: AppColours.white,
+  );
+  static const TextStyle size18Promptsemibold = TextStyle(
+    fontFamily: 'Prompt',
+    fontSize: 18,
+    fontWeight: FontWeight.w600, //semibold
+    color: AppColours.black,
+  );
+  static const TextStyle size14Promptregular = TextStyle(
+    fontFamily: 'Prompt',
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColours.grey,
+  );
+  static const TextStyle size14Promptlight = TextStyle(
+    fontFamily: 'Prompt',
+    fontSize: 14,
+    fontWeight: FontWeight.w300, //light
+    color: AppColours.white,
+  );
+  static const TextStyle size14Promptlightyellow = TextStyle(
+    fontFamily: 'Prompt',
+    fontSize: 14,
+    fontWeight: FontWeight.w300,
+    color: AppColours.yellow,
+  );
+}
