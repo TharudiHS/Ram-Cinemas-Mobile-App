5414005b2ba6d8caeba53ce3b1706a3d9e302091
TharudiHS <tharudihimeshaaa@gmail.com>
2025-11-18 12:41:18 +0530

    modified the colours

--- lib/utils/app_colours.dart
+++ lib/utils/app_colours.dart
@@ -1,34 +1,28 @@
 import 'package:flutter/material.dart';
 
 class AppColours {
-  static const charcoal = Color(0xFF141414);
-  static const blackTransparent = Color(0x29000000);
-  static const pureBlack = Color(0xFF000000);
-  static const goldTransparent50 = Color(0x80D9A935);
-  static const goldTransparent52 = Color(0x85D9A935);
-  static const bronze = Color(0xFFC2821D);
-  static const goldenYellow = Color(0xFFDCAE38);
-  static const deepGold = Color(0xFFB8730F);
-  static const mediumGray = Color(0xFF9A9A9A);
-  static const darkGray = Color(0xFF484848);
-  static const brightGold = Color(0xFFEEC643);
-  static const goldTransparent00 = Color(0x00D9A935);
-  static const classicGold = Color(0xFFD9A935);
-  static const nearBlack = Color(0xFF101010);
-  static const goldTransparent30 = Color(0x4DD9A935);
-  static const amber = Color(0xFFB8730F);
-  static const whiteTransparent = Color(0x00FFFFFF);
-  static const blackTransparent60 = Color(0x99000000);
-  static const blackTransparent80 = Color(0xCC000000);
-  static const goldTransparent80 = Color(0xCCEEC643);
-  static const goldTransparent40 = Color(0x66EEC643);
-  static const mutedGold = Color(0xFF8E6B17);
-  static const warmBronze = Color(0xFFB8730F);
-  static const lightGray = Color(0xFFB1B1B1);
-  static const softBlack = Color(0xFF191919);
-  static const blackTransparent30 = Color(0x4D000000);
-  static const gray = Color(0xFF4D4D4D);
-  static const ironGray = Color(0xFF484848);
-  static const darkGold = Color(0xFF4D3A11);
-  static const lightGrayTransparent = Color(0x00E2E2E2);
+  // Basic Colors
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color whiteTransparent = Color(0x00FFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color darkGrey = Color(0xFF141414);
+  static const Color almostBlack = Color(0xFF101010);
+
+  // Transparent and Opacity Variants
+  static const Color black29 = Color(0x29000000);
+  static const Color gold00 = Color(0x00D9A935);
+  static const Color gold4D = Color(0x4DD9A935);
+  static const Color gold80 = Color(0x80D9A935);
+  static const Color gold85 = Color(0x85D9A935);
+
+  // Gold / Yellow Variants
+  static const Color gold = Color(0xFFD9A935);
+  static const Color goldLight = Color(0xFFDCAE38);
+  static const Color goldDark = Color(0xFFC2821D);
+  static const Color bronze = Color(0xFFB8730F);
+  static const Color yellow = Color(0xFFEEC643);
+
+  // Greys
+  static const Color grey = Color(0xFF9A9A9A);
+  static const Color greyDark = Color(0xFF484848);
 }
