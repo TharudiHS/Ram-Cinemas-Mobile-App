8e61eee9615e99933cfc3c814bab4270bda5ae68
TharudiHS <tharudihimeshaaa@gmail.com>
2025-11-20 12:58:11 +0530

    added my account page

---
+++ lib/screens/account_screens/myaccount_page.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:ram_cinemas/components/app_bar.dart';
+import 'package:ram_cinemas/components/bottom_nav_bar.dart';
+import 'package:ram_cinemas/utils/app_colours.dart';
+import 'package:ram_cinemas/utils/text_styles.dart';
+
+class MyAccountPage extends StatelessWidget {
+  const MyAccountPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: CustomAppBar(title: "MY ACCOUNT"),
+      bottomNavigationBar: const BottomNavBar(selectedIndex: 3),
+      body: Stack(
+        children: [
+          //  BACKGROUND IMAGE
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/background_image.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // PROFILE INFO TITLE
+                Text(
+                  "PROFILE INFORMATION",
+                  style: TextStyles.size16Promptmediumwhite,
+                ),
+                const SizedBox(height: 16),
+
+                // PROFILE DETAILS
+                _buildRow("First Name:", "Kanchani"),
+                _buildRow("Second Name:", "Rathnayake"),
+                _buildRow("Email:", "kanchanirathnayake@gmail.com"),
+                _buildRow("Mobile Number:", "07771287198"),
+                _buildRow("Gender:", "Female"),
+                _buildRow("NIC Number:", "211201209120123V"),
+                _buildRow(
+                  "Address:",
+                  "27/2, New Hospital Road,\nKurunegala, Sri Lanka",
+                ),
+
+                const SizedBox(height: 10),
+
+                //  EDIT PROFILE
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    "Edit Profile",
+                    style: TextStyles.size14Promptlightgold.copyWith(
+                      color: AppColours.gold,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 30),
+
+                Divider(
+                  color: AppColours.greyDark.withOpacity(0.4),
+                  thickness: 1,
+                ),
+
+                const SizedBox(height: 20),
+
+                //  PRIVACY TITLE
+                Text("PRIVACY", style: TextStyles.size16Promptmediumwhite),
+
+                const SizedBox(height: 16),
+
+                // PASSWORD FIELD
+                _buildRow("Password:", "********"),
+
+                const SizedBox(height: 10),
+
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    "Change Password",
+                    style: TextStyles.size14Promptlightgold.copyWith(
+                      color: AppColours.gold,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 50),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // REUSABLE ROW BUILDER WIDGET
+
+  Widget _buildRow(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 120,
+            child: Text(label, style: TextStyles.size14Promptlight),
+          ),
+          Expanded(child: Text(value, style: TextStyles.size14Promptmeduim)),
+        ],
+      ),
+    );
+  }
+}
