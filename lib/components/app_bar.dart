0b034ab430cea10c372c9ca7e88f8a67f4247a55
TharudiHS <tharudihimeshaaa@gmail.com>
2025-11-20 12:57:44 +0530

    added app bar file

---
+++ lib/components/app_bar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:ram_cinemas/utils/app_colours.dart';
+import 'package:ram_cinemas/utils/text_styles.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final bool showBackArrow;
+  final VoidCallback? onMenuTap;
+  final VoidCallback? onBackTap;
+
+  const CustomAppBar({
+    super.key,
+    required this.title,
+    this.showBackArrow = false,
+    this.onMenuTap,
+    this.onBackTap,
+  });
+
+  @override
+  Size get preferredSize => const Size.fromHeight(78);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 78,
+      color: AppColours.almostBlack,
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // MENU ICON
+          GestureDetector(
+            onTap: onMenuTap,
+            child: const Icon(Icons.menu, color: AppColours.white, size: 30),
+          ),
+
+          const SizedBox(width: 16),
+
+          //  BACK ARROW (Gold)
+          if (showBackArrow)
+            GestureDetector(
+              onTap: onBackTap ?? () => Navigator.pop(context),
+              child: const Icon(
+                Icons.arrow_back_ios_new,
+                color: AppColours.gold,
+                size: 18,
+              ),
+            ),
+
+          if (showBackArrow) const SizedBox(width: 12),
+
+          // TITLE
+          Text(title, style: TextStyles.size16Promptmedium),
+        ],
+      ),
+    );
+  }
+}
