42cf9a0a18dbd53bce814806cc714387ccd458b4
TharudiHS <tharudihimeshaaa@gmail.com>
2025-11-25 21:45:16 +0530

    added movie model

---
+++ lib/data/model/movie_model.dart
@@ -0,0 +1,13 @@
+class MovieModel {
+  final String title;
+  final String subtitle;
+  final List<String> formats;
+  final String poster;
+
+  MovieModel({
+    required this.title,
+    required this.subtitle,
+    required this.formats,
+    required this.poster,
+  });
+}
